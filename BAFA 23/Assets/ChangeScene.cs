d60ad67fc6eaa05359daca3d0a5c42919ee7f823
theEpicNoob1 <shenb@wellingtoncollege.org.uk>
2023-01-19 10:16:18 +0000

    download firebase

--- BAFA 23/Assets/ChangeScene.cs
+++ BAFA 23/Assets/ChangeScene.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
     public void Change_to_scene(int scene_to_change_to) {
-        Application.LoadLevel(scene_to_change_to);
+        SceneManager.LoadScene(scene_to_change_to);
     }
 }
---
+++ BAFA 23/Assets/User.cs
@@ -0,0 +1,11 @@
+public class User
+{
+    public string name;
+    public string password;
+
+    public User(string name, string password)
+    {
+        this.name = name;
+        this.password = password;
+    }
+}
